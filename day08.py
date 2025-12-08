24b03bb33a13773856e1b116c52d38e77b5f42d8
Jason Wallestad <jason.wallestad@gmail.com>
2025-12-08 17:02:17 -0600

    adjustments

--- day08.py
+++ day08.py
@@ -9,14 +9,12 @@ for b1, box1 in enumerate(boxes):
     for b2 in range(b1 + 1, len(boxes)):
         distances.append((b1, b2, math.dist(box1, boxes[b2])))
 
-distances.sort(key=lambda x: x[2])
-for i in range(len(distances)):
-    b1, b2, distance = distances[i]
+for i, (b1, b2, distance) in enumerate(sorted(distances, key=lambda x: x[2])):
     connection = {b1, b2}
     for c, circuit in enumerate(circuits):
         if connection & circuit:
             circuits[c].update(connection)
-            if i <= p1_limit - 1 and connection <= merged_index:
+            if i <= p1_limit - 1:
                 for cc, circuit_check in enumerate(circuits):
                     if c != cc and connection & circuit_check:
                         circuits[c].update(circuits.pop(cc))
