0bb39c2c0e4ed89033884a532279225565b38bde
Jason Wallestad <jason.wallestad@gmail.com>
2025-12-17 19:17:07 -0600

    removed @cache

--- day11.py
+++ day11.py
@@ -1,15 +1,16 @@
-from functools import cache
-
 with open("input/day11.txt", "r") as puzzleInput:
     map = {str(val[0]):val[1].strip().split(' ') for val in [line.strip().split(':') for line in puzzleInput]}
 
-@cache
-def find_paths(pos, fft, dac):
+def find_paths(pos, fft, dac, memo):
     if pos == 'out':
         return 1 if fft and dac else 0
+    if (pos, fft, dac) in memo:
+        return memo[(pos, fft, dac)]
     fft = True if pos == 'fft' else fft
     dac = True if pos == 'dac' else dac
-    return (sum(find_paths(step, fft, dac) for step in map[pos]))
+    paths = sum(find_paths(step, fft, dac, memo) for step in map[pos])
+    memo[(pos, fft, dac)] = paths
+    return paths
 
-print(f"Part one: {find_paths('you', True, True)}")
-print(f"Part two: {find_paths('svr', False, False)}")
+print(f"Part one: {find_paths('you', True, True, {})}")
+print(f"Part two: {find_paths('svr', False, False, {})}")
